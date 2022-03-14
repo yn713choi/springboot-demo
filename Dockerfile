6266e4205dbab087a4a86b2e2c7e901af3539e7c
young713 <young713@young713ui-iMac-Pro.local>
2022-03-14 16:51:07 +0900

    logback add, controller add

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ COPY build.gradle .
 COPY settings.gradle .
 COPY src src
 RUN chmod +x ./gradlew
-RUN ./gradlew bootJar
+RUN ./gradlew bootJar 
 
 
 FROM openjdk:11.0.13-jdk
@@ -20,3 +20,37 @@ COPY --from=builder build/libs/*.jar app.jar
 ENV PROFILE=local
 
 ENTRYPOINT ["java", "-Dspring.profiles.active=${PROFILE}", "-jar", "app.jar"]
+
+
+# FROM openjdk:11.0.13-jdk as dependency
+
+# WORKDIR /workspace/app
+# COPY gradlew .
+# COPY gradle gradle
+# COPY build.gradle .
+# COPY settings.gradle .
+# # COPY src src
+# RUN chmod +x ./gradlew
+# RUN ./gradlew buildDependents -DskipTests
+
+# FROM openjdk:11.0.13-jdk as builder
+# WORKDIR /workspace/app
+
+# COPY --from=dependency /root/.m2/ /root/.m2/
+# COPY gradlew .
+# COPY gradle gradle
+# COPY build.gradle .
+# COPY settings.gradle .
+# COPY src src
+# RUN ./gradlew bootJar -DskipTests
+
+# FROM openjdk:11.0.13-jdk
+# #time
+# ENV TZ=Asia/Seoul
+# RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+# COPY --from=builder build/libs/*.jar app.jar
+
+# ENV PROFILE=local
+
+# ENTRYPOINT ["java", "-Dspring.profiles.active=${PROFILE}", "-jar", "app.jar"]
---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,57 @@
+package com.example.demo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller; 
+import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.net.InetAddress; 
+
+import java.net.UnknownHostException;
+
+@Controller 
+public class DemoController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    
+    @RequestMapping(value = "/demo") 
+    public String demo(){ 
+        return "index.html"; 
+    }
+    
+    @ResponseBody 
+    @RequestMapping("/valueTest") 
+    public String valueTest(){
+         String value = "테스트 String"; 
+         return value; 
+    }
+
+    @RequestMapping(value = "/logback") 
+    public ModelAndView test() throws Exception{ 
+        logger.trace("Trace Level 테스트");
+        logger.debug("DEBUG Level 테스트");
+        logger.info("INFO Level 테스트"); 
+        logger.warn("Warn Level 테스트"); 
+        logger.error("ERROR Level 테스트"); 
+        return null;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/hostname") 
+    public String getHostname(){ 
+        String hostname=null;
+        String hostAddress=null;
+        try{ 
+            hostname = InetAddress.getLocalHost().getHostName();
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+            System.out.println( "sysout ::: " + hostname ); 
+            System.out.println( "sysout ::: " + hostAddress ); 
+            logger.debug("logger {}", hostname);
+            logger.debug("logger {}", hostAddress);
+        } catch( UnknownHostException e ){
+             e.printStackTrace(); 
+        } 
+        return hostname + ", " + hostAddress;
+    } 
+}
